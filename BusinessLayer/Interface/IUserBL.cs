4f301832bc50c1bcfa85cd94d0b9199864c83c8a
Vikas ghodke <vikasghodke468@gmail.com>
2024-03-04 15:02:14 +0530

    Second Commit

---
+++ BusinessLayer/Interface/IUserBL.cs
@@ -0,0 +1,17 @@
+﻿using ModelLayer;
+using RepoLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBL
+    {
+        public UserEntity AddUserDetail(UserModel userModel);
+
+        //public UserEntity ViewDetail(string Email, string Password);
+
+        public string UserLogin(UserLogin userLogin);
+    }
+}
---
+++ RepoLayer/Interface/IUserRL.cs
@@ -0,0 +1,21 @@
+﻿using ModelLayer;
+using RepoLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepoLayer.Interface
+{
+    public interface IUserRL
+    {
+        public UserEntity AddUserDetail(UserModel userModel);
+
+        //public UserEntity ViewDetail(string Email, string Password);
+
+        public string UserLogin(UserLogin userLogin);
+
+
+
+
+    }
+}
